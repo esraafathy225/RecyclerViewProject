ac57cef31bfbc68c370b1f8bc15d0c68f84a5b91
esraa.fathy225 <esraa.fathy225@gmail.com>
2024-08-07 23:14:12 +0300

    AppBar and Navigation Drawer

--- app/src/main/java/com/example/recyclerviewproject/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewproject/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.recyclerviewproject
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -17,7 +19,7 @@ import com.example.myapplication.User
 import com.example.myapplication.UserAdapter
 import com.example.recyclerviewproject.databinding.ActivityMainBinding
 import com.example.recyclerviewproject.ui.theme.RecyclerViewProjectTheme
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -27,6 +29,7 @@ class MainActivity : ComponentActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view: View = binding.root
         setContentView(view)
+
 
         //binding.name="Hiii"
 
@@ -38,15 +41,64 @@ class MainActivity : ComponentActivity() {
             User("Menna Ali","Menna@gmail.com"),
             User("Youssef Mohamed","Youssef@gmail.com"),
             User("Ahmed Mohamed","Ahmed@gmail.com"),
-            User("Mahmoud Mohamed","Mahmoud@gmail.com"),
-            User("Mona Ahmed","Mona@gmail.com"),
-            User("Menna Ali","Menna@gmail.com"),
-            User("Youssef Mohamed","Youssef@gmail.com")
+            User("Mahmoud Ahmed","Mahmoud@gmail.com"),
+            User("Yasmine Ahmed","Mona@gmail.com"),
+            User("Mohamed Ali","Menna@gmail.com"),
+            User("Farida Mohamed","Youssef@gmail.com")
         )
 
-        val userAdapter = UserAdapter(users)
-        binding.rvUsers.layoutManager= LinearLayoutManager(this)
+
+
+       val userAdapter = UserAdapter(users)
+       // binding.rvUsers.layoutManager=LinearLayoutManager(this)
         binding.rvUsers.adapter=userAdapter
+
+        binding.toolbar.materialToolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.search -> {
+                    Toast.makeText(this,R.string.search_clicked, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.profile -> {
+                    Toast.makeText(this,R.string.profile_clicked, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.settings -> {
+                    Toast.makeText(this,R.string.settings_clicked, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+        binding.toolbar.materialToolbar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+        }
+
+
+        binding.navigationView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    Toast.makeText(this, R.string.home_clicked, Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.close()
+                    true
+                }
+
+                R.id.gallery -> {
+                    Toast.makeText(this, R.string.gallery_clicked, Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.close()
+                    true
+                }
+                R.id.slideshow -> {
+                    Toast.makeText(this, R.string.slideshow_clicked, Toast.LENGTH_SHORT).show()
+                    binding.drawerLayout.close()
+                    true
+                }
+
+                else -> false
+            }
+        }
 
     }
 }
